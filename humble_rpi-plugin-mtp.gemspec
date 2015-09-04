0894cf79b3bee46ff213753568ee1204ec018fea
Your Name <you@example.com>
2015-09-04 16:15:13 +0100

    feature: When the message is a URL the body of the message is fetched from the address.

--- humble_rpi-plugin-mtp.gemspec
+++ humble_rpi-plugin-mtp.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'humble_rpi-plugin-mtp'
-  s.version = '0.1.2'
+  s.version = '0.2.0'
   s.summary = 'This Humble RPi plugin controls the mini thermal printer.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/humble_rpi-plugin-mtp.rb']
   s.signing_key = '../privatekeys/humble_rpi-plugin-mtp.pem'
-  s.add_runtime_dependency('serialport_mtp', '~> 0.1', '>=0.1.0')
+  s.add_runtime_dependency('serialport_mtp', '~> 0.1', '>=0.1.1')
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
--- lib/humble_rpi-plugin-mtp.rb
+++ lib/humble_rpi-plugin-mtp.rb
@@ -2,25 +2,26 @@
 
 # file: humble_rpi-plugin-mtp.rb
 
-
+require 'open-uri'
 require 'serialport_mtp'
 
 
 class HumbleRPiPluginMTP < SerialPortMTP
 
   def initialize(settings: {}, variables: {})
-
+    
     h = {port: "/dev/ttyAMA0", baud_rate: 19200}.merge settings
+    @username, @password = settings[:username], settings[:password]
     device_id = variables[:device_id] || 'pi'
     
     super(port: h[:port], baud_rate: h[:baud_rate])
 
   end
 
-  def on_printer_message(message)
-    
+  def on_printer_message(raw_message)
+
     wake
-    
+
     inverse_on
     bold_on
     println Time.now.to_s
@@ -28,8 +29,9 @@ class HumbleRPiPluginMTP < SerialPortMTP
     inverse_off
     
     feed
-    
-    println message
+
+    message = raw_message[/^https?:\/\//] ? fetch(raw_message) : raw_message
+    message.lines {|x| self.print x}
     feed 4
     
     sleep_after 10 # seconds
@@ -42,6 +44,14 @@ class HumbleRPiPluginMTP < SerialPortMTP
     
   end
   
-  alias on_start start  
+  alias on_start start
+
+  private
+  
+  def fetch(url)
+    
+    open(url, http_basic_authentication: [@username, @password]).read
+    
+  end
   
 end
